f65378803d4258e010634c882a497ef502c83bd6
Arik Gadye <arikdovgadye@gmail.com>
2017-05-22 12:01:52 +0200

    changed home page video

--- js/index.js
+++ js/index.js
@@ -1,24 +1,11 @@
-// $(document).ready(function(){
-//     animateDiv(); 
-// });
+$(document).ready(function(){
+	spinText();
+});
 
-// function makeNewPosition(){
-    
-//     // Get viewport dimensions (remove the dimension of the div)
-//     var h = $(window).height() - 50;
-//     var w = $(window).width() - 50;
-    
-//     var nh = Math.floor(Math.random() * h);
-//     var nw = Math.floor(Math.random() * w);
-    
-//     return [nh,nw];    
-    
-// }
 
-// function animateDiv(){
-//     var newq = makeNewPosition();
-//     $('.a').animate({ top: newq[0], left: newq[1] }, function(){
-//       animateDiv();        
-//     });
-    
-// };
+function spinText() {
+	var firstH1 = $('h1').first();
+	var secondH1 = $('h1:nth-child(2)');
+	firstH1.attr("id", "one");
+	secondH1.attr("id", "two");
+};
