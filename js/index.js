46875d46056c473ca951cf9dbf3a3fafd2a3f976
Arik Gadye <arikdovgadye@gmail.com>
2017-06-06 13:52:08 +0200

    add some js, add logo to homepage

--- js/index.js
+++ js/index.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
 function spinText() {
 	var firstH1 = $('h1').first();
 	var secondH1 = $('h1:nth-child(2)');
+	var img = $("img").first();
 	firstH1.attr("id", "one");
 	secondH1.attr("id", "two");
+	if (window.location.href.includes("press")) {
+		return;
+	};
+	img.attr("id", "one");
 };
