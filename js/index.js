ebc224397f64b35fdbe733326a88247ce150cc72
Arik Gadye <arikdovgadye@gmail.com>
2017-06-21 16:14:52 +0200

    new layout

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-	spinText();
-});
+// $(document).ready(function(){
+// 	spinText();
+// });
 
 
-function spinText() {
-	var firstH1 = $('h1').first();
-	var secondH1 = $('h1:nth-child(2)');
-	var img = $("img").first();
-	firstH1.attr("id", "one");
-	secondH1.attr("id", "two");
-	if (window.location.href.includes("press")) {
-		return;
-	};
-	img.attr("id", "one");
-};
+// function spinText() {
+// 	var firstH1 = $('h1').first();
+// 	var secondH1 = $('h1:nth-child(2)');
+// 	var img = $("img").first();
+// 	firstH1.attr("id", "one");
+// 	secondH1.attr("id", "two");
+// 	if (window.location.href.includes("press")) {
+// 		return;
+// 	};
+// 	img.attr("id", "one");
+// };
