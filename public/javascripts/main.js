4c2051e8397514521d23e53d5df8b0f6c148a85c
Arik Gadye <arikdovgadye@gmail.com>
2018-07-19 01:16:21 +0200

    jo

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-	$(".carousel").carousel("pause")
+$(document).ready(function(){
+    animateDiv();
+    
 });
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 20;
+    var w = $(window).width() - 20;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    $('.animated').animate({ top: newq[0], left: newq[1] }, 1000, function(){
+      animateDiv();        
+    });
+    
+};
